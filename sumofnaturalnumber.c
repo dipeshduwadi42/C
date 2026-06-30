669f2d2320efde73aac54812dccfd7f62aa495ef
dipeshduwadi42 <dipeshduwadi42@gmail.com>
2026-06-30 21:57:48 +0545

    Update sumofnaturalnumber.c

--- sumofnaturalnumber.c
+++ sumofnaturalnumber.c
@@ -6,7 +6,7 @@ int main()
     scanf("%d",&n);
     for(i = 1; i <= n; i++)
     {
-        sum = sum + i;
+        sum = sum + i;//repeates the values
     }
     printf("The sum of natural numbers  is %d", sum);
     return 0;
